023132fe761c33c55526ddb3cb42be8bc9d83652
carlossp04 <carlossanchezportillo04@gmail.com>
2026-06-01 14:37:05 +0200

    limpieza archivos muertos

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -11,28 +11,4 @@ export const userColors = [
   '#84cc16',
 ];
 
-export const templateText = `Titulo día de entreno (Opcional)
-
-Nombre Ejercicio
-Series x Repes x Peso
-
-##   Formatos Válidos   ##
-## para los ejercicios: ##
-
-Opción 1 (Detallada):
-4s x 10r x 20.5kg
-
-Opción 2 (Rápida):
-4x10x20,5
-
-*** Ejemplo ***
-
-Dia de brazo
-
-Predicador (barra z de 9kg + 10kg por lado)
-4 x 10 x 29
-
-Curl Biceps con mancuernas de 10kg
-4s x 10r x 20kg`;
-
 export { initialTrainingText };
